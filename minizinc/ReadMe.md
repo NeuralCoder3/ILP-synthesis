9b7857f9787962e96d4e56fe4f9f010941865a91
Marcel Ullrich <ullrich@cs.uni-saarland.de>
2024-07-26 16:48:37 +0200

    update & minmax synth

--- minizinc/ReadMe.md
+++ minizinc/ReadMe.md
@@ -16,9 +16,10 @@ Use `-a` to print all solutions (or intermediate solutions if solve satisfiable)
 `time minizinc --solver Chuffed -a sort_syn.mzn sort_data.dzn | tee -a sort_sol_11_chuffed_a_order.txt`
 
 To try:
-- systematic other solvers
-- `O0` ... `O5` (`O5` does not seem to improve optimizing query)
+- [ ] systematic other solvers
+- [ ] `O0` ... `O5` (`O5` does not seem to improve optimizing query)
 - [x] enforce output 123 (30s-1min -> 5s)
+- [ ] compute critical path for solutions (dependency DAG)
 
 
 With consistent renaming, every permutation should be possible as output
@@ -30,7 +31,70 @@ All programs:
   - all: 5602
   - max mov, cmp: 2520
   - time: 13m
+  - minimal raw: 680
+  - minimal raw+war+waw: 576
 - Without output order:
   - all: 33612
   - max mov, cmp: 15120
   - time: 154m
+
+
+How to extract:
+
+
+
+
+
+
+
+
+
+time minizinc --solver Chuffed sort_syn_v2_org_change.mzn sort_data3.dzn
+
+time minizinc --solver Chuffed sort_syn_mixed.mzn sort_data3.dzn
+
+
+
+Available solver configurations:
+  Chuffed 0.12.1 (org.chuffed.chuffed, cp, lcg, int)
+  COIN-BC 2.10.10/1.17.8 (org.minizinc.mip.coin-bc, mip, float, api, osicbc, coinbc, cbc)
+  Couenne 0.5.8 (org.coin-or.couenne)
+  CPLEX <unknown version> (org.minizinc.mip.cplex, mip, float, api)
+  findMUS 0.7.0 (org.minizinc.findmus)
+  Gecode 6.3.0 (org.gecode.gecode, default solver, cp, int, float, set, restart)
+  Gecode Gist 6.3.0 (org.gecode.gist, cp, int, float, set, restart)
+  Globalizer 0.1.7.2 (org.minizinc.globalizer, experimental, tool)
+  Gurobi 10.0.3 (org.minizinc.mip.gurobi, mip, float, api)
+  HiGHS 1.5.1 (org.minizinc.mip.highs, mip, float, api, highs)
+  SCIP <unknown version> (org.minizinc.mip.scip, mip, float, api)
+  Xpress <unknown version> (org.minizinc.mip.xpress, mip, float, api)
+Search path for solver configurations:
+  /opt/minizinc-ide/share/minizinc/solvers
+  /usr/local/share/minizinc/solvers
+  /usr/share/minizinc/solvers
+
+
+
+Available solver configurations:
+  Chuffed 0.12.1 (org.chuffed.chuffed, cp, lcg, int)
+  COIN-BC 2.10.10/1.17.8 (org.minizinc.mip.coin-bc, mip, float, api, osicbc, coinbc, cbc)
+  Couenne 0.5.8 (org.coin-or.couenne)
+  CPLEX <unknown version> (org.minizinc.mip.cplex, mip, float, api)
+  findMUS 0.7.0 (org.minizinc.findmus)
+  Gecode 6.3.0 (org.gecode.gecode, default solver, cp, int, float, set, restart)
+  Gecode Gist 6.3.0 (org.gecode.gist, cp, int, float, set, restart)
+  Globalizer 0.1.7.2 (org.minizinc.globalizer, experimental, tool)
+  Gurobi 10.0.3 (org.minizinc.mip.gurobi, mip, float, api)
+  HiGHS 1.5.1 (org.minizinc.mip.highs, mip, float, api, highs)
+  OR Tools CP-SAT 9.8.9999 (com.google.ortools.sat, cpsatlp, cp, lcg, int)
+  SCIP <unknown version> (org.minizinc.mip.scip, mip, float, api)
+  Xpress <unknown version> (org.minizinc.mip.xpress, mip, float, api)
+
+
+
+gecode >950min?
+gurobi >166min
+ortools >72min
+
+
+com.google.ortools.sat =58min
--- minizinc/extract_programs.py
+++ minizinc/extract_programs.py
@@ -2,9 +2,15 @@ import os
 import sys
 import typing
 
-file = "sort_sol_11_chuffed_a.txt"
-# file = "sort_sol_11_chuffed_a_order.txt"
+# file = "sort_sol_11_chuffed_a.txt"
+file = "result/sort_sol_11_chuffed_a_order.txt"
+# file = "result/sort_sol_11_chuffed_a.txt"
+output = "output"
+if not os.path.exists(output):
+    os.makedirs(output)
 length = 11
+n = 3
+s = 1
 
 programs = []
 current_program = []
@@ -28,6 +34,17 @@ for l in content:
             programs.append(current_program)
             inProgram = False
     
+seenPrograms = set()
+new_programs = []
+for p in programs:
+    sp = "\n".join(p)
+    if sp in seenPrograms:
+        continue
+    seenPrograms.add(sp)
+    new_programs.append(p)
+
+# print(len(programs))
+programs = new_programs
 print(len(programs))
 
 # sort by count of 1*mov + 2*cmp + 3*cmov
@@ -60,10 +77,16 @@ def programScore(program:list[tuple[str,int,int]]):
     program_score = sum([score(i) for i in instrs])
     return program_score
 
+def printProgram(program:list[tuple[str,int,int]]):
+    for i,a,b in program:
+        print(f"{i} {a} {b}")
+        
+def programToString(program:list[tuple[str,int,int]]):
+    return "\n".join([f"{i} {a} {b}" for i,a,b in program])
+
 programs = [parseProgram(p) for p in programs]
 best = min(programs, key=programScore)
-for i, a, b in best:
-    print(f"{i} {a} {b}")
+# printProgram(best)
     
 movs = sum([1 for i,_,_ in best if i=="mov"])
 cmps = sum([1 for i,_,_ in best if i=="cmp"])
@@ -71,8 +94,122 @@ cmovs = sum([1 for i,_,_ in best if i.startswith("cmov")])
 print(f"{movs} Move instructions, {cmps} Compare instructions, {cmovs} Conditional Move instructions")
 
 
+original_programs = programs
 programs = sorted(programs, key=programScore)
 # get all programs with the minimal score
 best_score = programScore(programs[0])
 best = [p for p in programs if programScore(p)==best_score]
 print(f"There are {len(best)} programs with the best score of {best_score}")
+
+less_flag = n+s
+greater_flag = n+s+1
+flags = [less_flag, greater_flag]
+
+def getReads(command:tuple[str,int,int]) -> list[int]:
+    i,a,b = command
+    if i == "cmp":
+        return [a,b]
+    elif i.startswith("cmov"):
+        return [b]+flags
+    elif i == "mov":
+        return [b]
+    else:
+        assert False, f"Unknown instruction {i}"
+
+def getWrites(command:tuple[str,int,int]) -> list[int]:
+    i,a,b = command
+    if i == "cmp":
+        return flags
+    elif i.startswith("cmov"):
+        return [a]
+    elif i == "mov":
+        return [a]
+    else:
+        assert False, f"Unknown instruction {i}"
+        
+def hasDependency(cmd1, cmd2, raw=True, waw=False, war=False):
+    # raw: read after write
+    # waw: write after write
+    # war: write after read
+    if raw:
+        reads1 = getReads(cmd1)
+        writes2 = getWrites(cmd2)
+        if any(r in writes2 for r in reads1):
+            return True
+    if waw:
+        writes1 = getWrites(cmd1)
+        writes2 = getWrites(cmd2)
+        if any(w in writes2 for w in writes1):
+            return True
+    if war:
+        writes1 = getWrites(cmd1)
+        reads2 = getReads(cmd2)
+        if any(w in reads2 for w in writes1):
+            return True
+    return False
+
+def depGraph(program, *args, **kwargs):
+    graph = []
+    for i,cmd1 in enumerate(program):
+        graph.append([])
+        for j,cmd2 in enumerate(program):
+            if j<=i:
+                continue
+            if hasDependency(cmd1, cmd2, *args, **kwargs):
+                graph[i].append(j)
+    return graph
+
+def criticalPath(depGraph):
+    n = len(depGraph)
+    visited = [False]*n
+    dp = [0]*n
+    
+    def dfs(u):
+        if visited[u]:
+            return dp[u]
+        visited[u] = True
+        for v in depGraph[u]:
+            dp[u] = max(dp[u], dfs(v)+1)
+        return dp[u]
+    
+    for u in range(n):
+        dfs(u)
+    
+    return max(dp)
+
+raw = True
+# waw = False
+# war = False
+waw = True
+war = True
+best = [(p, criticalPath(depGraph(p, raw=raw, war=war, waw=waw))) for p in best]
+best = sorted(best, key=lambda x: x[1])
+print(f"The best program has a critical path of {best[0][1]}")
+print(f"The worst best program has a critical path of {best[-1][1]}")
+
+best_path = best[0][1]
+best = [p for p,s in best if s==best_path]
+print("There are", len(best), "programs with the best critical path of length", best_path)
+
+print("The best program is:")
+printProgram(best[0])
+
+# print("The worst best program is:")
+# printProgram(best[-1][0])
+
+
+count = {}
+for p in original_programs:
+    pscore = programScore(p)
+    code = programToString(p)
+    path = criticalPath(depGraph(p, raw=raw, war=war, waw=waw))
+    if pscore not in count:
+        count[pscore] = {}
+    if path not in count[pscore]:
+        count[pscore][path] = 0
+    count[pscore][path] += 1
+    outputPath = os.path.join(output, str(pscore), str(path), str(count[pscore][path])+".txt")
+    os.makedirs(os.path.dirname(outputPath), exist_ok=True)
+    with open(outputPath, "w") as f:
+        f.write(code)
+        
